af32a614e93858ffc552f5500c7ab6d877972e7b
eboss <emanuele.boscari@studenti.unitn.it>
2020-05-20 22:52:21 +0000

    Added custom header and response error

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Fuck you git nigga');
+  res.send('Hello there customer!');
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
 
+app.get('/demo', (req, res) => {
+  res.set('X-stack', 'you found the custom header');
+  res.status(418);
+  res.sedn("Can't listen im getting some sleep");
+});
 
